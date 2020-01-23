a5ce04bd67f5d1fb1a2127c3c7bd51b17c36c8ca
unknown <yonoraphael@gmail.com>
2020-01-23 19:37:55 +0700

    Updates

---
+++ precache-manifest.7928a28459ea113100ce0904c799ca0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee40bc6ec9d03abf90f5f56ae542000",
+    "url": "/siapsedia/index.html"
+  },
+  {
+    "revision": "74ff2711014df5d90c7b",
+    "url": "/siapsedia/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5eb099c2e69315e3e58c",
+    "url": "/siapsedia/static/js/2.c78ace04.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/siapsedia/static/js/2.c78ace04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74ff2711014df5d90c7b",
+    "url": "/siapsedia/static/js/main.543d5a9e.chunk.js"
+  },
+  {
+    "revision": "6d6ed19598e3de6f9f3a",
+    "url": "/siapsedia/static/js/runtime-main.4fd799f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/siapsedia/static/media/logo.5d5d9eef.svg"
+  }
+]);
